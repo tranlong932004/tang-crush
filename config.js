825f60e2fa5fd8dc5916dc6b31c2f769996efe1d
tranlong932004 <85268359+tranlong932004@users.noreply.github.com>
2021-06-03 09:59:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Anh biết là anh hay bận nhưng mà anh có thời gian là anh ib cho bé ngay nên là đừng xa lánh anh nha 🥰',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100057057488560' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
